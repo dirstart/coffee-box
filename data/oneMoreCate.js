73ddcbf2e7149239d4af49cdc3c22e6f11b00070
dirstart <835041058@qq.com>
2024-11-02 11:47:56 +0800

    feat: 增加 mockData 数据

---
+++ data/oneMoreCate.js
@@ -0,0 +1,409 @@
+// 咖啡
+const arr1 = [{
+  "productId": "117",
+  "productName": "拿铁",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group3/M00/5E/21/CtxgFGZ76i6AUN1MAAITQmZpPdU211.png"
+},
+{
+  "productId": "125",
+  "productName": "焦糖玛奇朵",
+  "thumbnailUrl": "https://img04.luckincoffeecdn.com/group3/M00/5E/38/CtxgFGZ77HiAMBzxAAISoQKWKG0143.png"
+},
+{
+  "productId": "121",
+  "productName": "卡布奇诺",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group2/M00/B3/16/CtwiPWHnwW6AL4KjAAH1Y8uFSSc827.png"
+},
+{
+  "productId": "127",
+  "productName": "香草拿铁",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group3/M00/5E/37/CtxgFGZ77FyAK1qfAAIJ_l_C5E4909.png"
+},
+{
+  "productId": "124",
+  "productName": "精萃澳瑞白",
+  "thumbnailUrl": "https://img06.luckincoffeecdn.com/group3/M00/5E/3A/CtxgFGZ77KqAO-AHAAH5MljksPM292.png"
+},
+{
+  "productId": "2500",
+  "productName": "标准美式",
+  "thumbnailUrl": "https://img02.luckincoffeecdn.com/group1/M00/2F/26/CtwhA2Hnw-GABuwhAAIZH9vQLIg476.png"
+},
+{
+  "productId": "2507",
+  "productName": "加浓美式",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group2/M00/B3/21/CtwiPGHnxAiAd6UwAAIdb0ZJv1E742.png"
+},
+{
+  "productId": "1800",
+  "productName": "丝绒拿铁",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group1/M00/75/A2/CtwhAmGTC7qAc2ddAAIcRCRcoYE360.png"
+},
+{
+  "productId": "4652",
+  "productName": "太妃榛香拿铁",
+  "thumbnailUrl": "https://img02.luckincoffeecdn.com/group2/M00/13/10/CtwiPGcYlySAC5YrAAHqfFoJKwU431.png"
+},
+{
+  "productId": "4596",
+  "productName": "小黄油美式",
+  "thumbnailUrl": "https://img06.luckincoffeecdn.com/group3/M00/A5/12/CtxgFGcVI7SAEQ2qAAJv9FuuRZ4709.png"
+},
+{
+  "productId": "379",
+  "productName": "陨石拿铁",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group2/M00/B3/1B/CtwiPGHnwqiAXXZYAAJ9wp7TBxI301.png"
+},
+{
+  "productId": "1884",
+  "productName": "生椰丝绒拿铁",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group2/M00/63/25/CtwiPGPyQP2AHvaTAAIU-J06nY0805.png"
+},
+{
+  "productId": "2143",
+  "productName": "香草丝绒拿铁",
+  "thumbnailUrl": "https://img01.luckincoffeecdn.com/group1/M00/75/A3/CtwhAmGTC7qADjI1AAIpJBx_k7I039.png"
+},
+{
+  "productId": "1344",
+  "productName": "陨石生椰拿铁",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/2F/2E/CtwhAmHnw8OAKS6OAAJt1InFWKI828.png"
+},
+{
+  "productId": "1262",
+  "productName": "生椰拿铁",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/2F/24/CtwhA2Hnw5-APh3XAAI1QTyCyOU499.png"
+},
+{
+  "productId": "1302",
+  "productName": "抹茶好喝椰",
+  "thumbnailUrl": "https://img01.luckincoffeecdn.com/group1/M00/FF/03/CtwhA2OqvFWANACGAAGpbqlOucU355.png"
+},
+{
+  "productId": "1227",
+  "productName": "抹茶拿铁",
+  "thumbnailUrl": "https://img02.luckincoffeecdn.com/group3/M00/5E/3C/CtxgFGZ77NOAAONiAAG3-n0KdV8571.png"
+},
+{
+  "productId": "4440",
+  "productName": "塞纳莓莓拿铁",
+  "thumbnailUrl": "https://img01.luckincoffeecdn.com/group3/M00/52/87/CtxgFGadIlWAJWdtAAKMQcrpxYU518.png"
+},
+{
+  "productId": "4577",
+  "productName": "青苹果丝绒拿铁",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group3/M00/3D/E6/CtxgFGbmU6KAb498AAJG-1UGd1E973.png"
+},
+{
+  "productId": "4615",
+  "productName": "瑰夏·美式",
+  "thumbnailUrl": "https://img01.luckincoffeecdn.com/group3/M00/B0/69/CtxgEmbt1n6AYTllAAHUz5ypd4U031.png"
+},
+{
+  "productId": "4616",
+  "productName": "瑰夏·拿铁",
+  "thumbnailUrl": "https://img01.luckincoffeecdn.com/group2/M00/5D/DD/CtwiPWMRxEqAWSBYAAGppnzo4l4661.png"
+},
+{
+  "productId": "4617",
+  "productName": "瑰夏·澳瑞白",
+  "thumbnailUrl": "https://img02.luckincoffeecdn.com/group2/M00/5D/DD/CtwiPGMRxIyAWzPnAAGSfvB31P0343.png"
+},
+{
+  "productId": "4618",
+  "productName": "瑰夏·Dirty",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group3/M00/B0/69/CtxgEmbt1gOAPtl1AAIC5xcv1c4774.png"
+},
+{
+  "productId": "4510",
+  "productName": "柚C美式（超大杯）",
+  "thumbnailUrl": "https://img02.luckincoffeecdn.com/group3/M00/B9/E7/CtxgFGZHT_yAaj-gAAH2mGUsJOM360.png"
+},
+{
+  "productId": "4511",
+  "productName": "橙C美式（超大杯）",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group3/M00/B9/DA/CtxgEmZHTreAWclUAAHLup7n_wg125.png"
+},
+{
+  "productId": "4512",
+  "productName": "柠C美式（超大杯）",
+  "thumbnailUrl": "https://img02.luckincoffeecdn.com/group3/M00/B9/DD/CtxgFGZHTxKAbbxZAAHYEJsEsBw739.png"
+},
+{
+  "productId": "4454",
+  "productName": "柠C美式",
+  "thumbnailUrl": "https://img04.luckincoffeecdn.com/group3/M00/86/F8/CtxgEmYrSRGAF4JtAAKDRdnJaCE696.png"
+},
+{
+  "productId": "4619",
+  "productName": "苹果C美式",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group3/M00/96/8A/CtxgFGcSPPWASmnRAAIlVBBN7J4900.png"
+},
+{
+  "productId": "4160",
+  "productName": "酱香拿铁",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group2/M00/80/00/CtwiPGVHv0KAVGGxAAJLvbAFMR4573.png"
+},
+{
+  "productId": "4610",
+  "productName": "黑巧布朗尼小曲",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group3/M00/D2/A8/CtxgEmbwGZKASFVjAAKMXGpE9X8818.png"
+},
+{
+  "productId": "4604",
+  "productName": "乌龙拿铁",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group3/M00/B8/1E/CtxgFGbdzeGAQ7fVAAKK_wU7qfI983.png"
+},
+{
+  "productId": "4500",
+  "productName": "燕麦拿铁",
+  "thumbnailUrl": "https://img01.luckincoffeecdn.com/group3/M00/5E/48/CtxgFGZ77feAOrjeAAIvfBrDxSA376.png"
+},
+{
+  "productId": "118",
+  "productName": "摩卡",
+  "thumbnailUrl": "https://img02.luckincoffeecdn.com/group3/M00/5E/3B/CtxgFGZ77MKAKKSOAAHlCfUYH_w371.png"
+},
+{
+  "productId": "119",
+  "productName": "巧克力瑞纳冰",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group1/M00/7E/74/CtwhA2N_J1qAQhFZAAJS9vRcSNU059.png"
+},
+{
+  "productId": "1226",
+  "productName": "生椰玛奇朵",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/2F/24/CtwhAmHnw5eAFuz8AAI_0cPmhSc064.png"
+},
+{
+  "productId": "2200",
+  "productName": "澳洲风味白咖啡",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/2F/29/CtwhAmHnw5yAZzjKAAIXfwMBmR4764.png"
+},
+{
+  "productId": "4611",
+  "productName": "太妃榛香美式",
+  "thumbnailUrl": "https://img04.luckincoffeecdn.com/group3/M00/5E/4E/CtxgFGaM_i2AMfSWAAIw8eBA27I169.png"
+}
+]
+
+// 瑞纳冰
+const arr2 = [{
+  "productId": "311",
+  "productName": "抹茶瑞纳冰",
+  "thumbnailUrl": "https://img02.luckincoffeecdn.com/group2/M00/04/14/CtwiPGN_Jy-AJFxMAAHhy_aGStk220.png"
+},
+{
+  "productId": "119",
+  "productName": "巧克力瑞纳冰",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group1/M00/7E/74/CtwhA2N_J1qAQhFZAAJS9vRcSNU059.png"
+},
+{
+  "productId": "298",
+  "productName": "咖啡瑞纳冰",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group2/M00/04/15/CtwiPGN_J4-AH0PxAAG6V_9Oe6A892.png"
+}]
+
+// 生椰系列
+const arr3 = [{
+  "productId": "1262",
+  "productName": "生椰拿铁",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/2F/24/CtwhA2Hnw5-APh3XAAI1QTyCyOU499.png"
+},
+{
+  "productId": "1884",
+  "productName": "生椰丝绒拿铁",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group2/M00/63/25/CtwiPGPyQP2AHvaTAAIU-J06nY0805.png"
+},
+{
+  "productId": "1344",
+  "productName": "陨石生椰拿铁",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/2F/2E/CtwhAmHnw8OAKS6OAAJt1InFWKI828.png"
+},
+{
+  "productId": "1302",
+  "productName": "抹茶好喝椰",
+  "thumbnailUrl": "https://img01.luckincoffeecdn.com/group1/M00/FF/03/CtwhA2OqvFWANACGAAGpbqlOucU355.png"
+},
+{
+  "productId": "1226",
+  "productName": "生椰玛奇朵",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/2F/24/CtwhAmHnw5eAFuz8AAI_0cPmhSc064.png"
+},
+{
+  "productId": "4596",
+  "productName": "小黄油美式（生椰）",
+  "thumbnailUrl": "https://img06.luckincoffeecdn.com/group3/M00/A5/12/CtxgFGcVI7SAEQ2qAAJv9FuuRZ4709.png"
+},
+{
+  "productId": "4668",
+  "productName": "生椰锡兰轻乳茶",
+  "thumbnailUrl": "https://img02.luckincoffeecdn.com/group1/M00/76/37/CtwhA2cVI2WAFWgUAAHPbwxC4fg751.png"
+},
+{
+  "productId": "4667",
+  "productName": "生椰锡兰轻乳茶（特大杯）",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/76/36/CtwhA2cVI0yAcXkDAAIX-fpCsKc649.png"
+},
+{
+  "productId": "4536",
+  "productName": "生椰桂花轻乳茶",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group3/M00/9F/70/CtxgFGcPjJCAQCnNAAJNaYdKKwA715.png"
+},
+{
+  "productId": "4390",
+  "productName": "生椰莓莓轻乳茶",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group3/M00/48/13/CtxgFGbIYCSAVKZDAAJg2yXXWKA135.png"
+},
+{
+  "productId": "4321",
+  "productName": "生椰芝芝莓莓",
+  "thumbnailUrl": "https://img06.luckincoffeecdn.com/group3/M00/34/20/CtxgEmYGrnyAHD1aAAJhUWD0HJE275.png"
+},
+{
+  "productId": "4362",
+  "productName": "生椰柠檬气泡饮",
+  "thumbnailUrl": "https://img05.luckincoffeecdn.com/group3/M00/36/8D/CtxgFGYKhXCAf0mTAAH2H59QlnY517.png"
+}
+]
+
+// 轻乳茶
+const arr4 = [{
+  "productId": "1001",
+  "productName": "经典奶茶",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/24/CtwhA2BfWvWABY1zAAHKR8I_YhE328.png"
+},
+{
+  "productId": "1002",
+  "productName": "红茶拿铁",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/25/CtwhA2BfWvSAVTlKAAHKR8I_YhE328.png"
+},
+{
+  "productId": "1003",
+  "productName": "抹茶轻乳茶",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/26/CtwhA2BfWvSAyZVAAHKR8I_YhE328.png"
+},
+{
+  "productId": "1004",
+  "productName": "草莓轻乳茶",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/27/CtwhA2BfWvOAGGzDAAHk0XWqU1E328.png"
+},
+{
+  "productId": "1005",
+  "productName": "芒果轻乳茶",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/28/CtwhA2BfWvOABxJDAAHk0XWqU1E328.png"
+},
+{
+  "productId": "1006",
+  "productName": "柠檬轻乳茶",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/29/CtwhA2BfWvCAWyLDAAHk0XWqU1E328.png"
+},
+{
+  "productId": "1007",
+  "productName": "蜜桃轻乳茶",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/30/CtwhA2BfWvSAYbGDAAHk0XWqU1E328.png"
+},
+{
+  "productId": "1008",
+  "productName": "荔枝轻乳茶",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/31/CtwhA2BfWvKAGU0DAAHk0XWqU1E328.png"
+},
+{
+  "productId": "1009",
+  "productName": "香蕉轻乳茶",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/32/CtwhA2BfWvKADPsDAAHk0XWqU1E328.png"
+},
+{
+  "productId": "1010",
+  "productName": "蓝莓轻乳茶",
+  "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/33/CtwhA2BfWvCAWcBDAAHk0XWqU1E328.png"
+}
+]
+
+// 小食
+const arr5 = [
+  {
+    "productId": "1101",
+    "productName": "提拉米苏",
+    "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/34/CtwhA2BfWvGA4B6SDAAHk0XWqU1E328.png"
+  },
+  {
+    "productId": "1102",
+    "productName": "芝士蛋糕",
+    "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/35/CtwhA2BfWvWABY1zAAHKR8I_YhE328.png"
+  },
+  {
+    "productId": "1103",
+    "productName": "巧克力慕斯",
+    "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/36/CtwhA2BfWvWABY1zAAHKR8I_YhE328.png"
+  },
+  {
+    "productId": "1104",
+    "productName": "水果塔",
+    "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/37/CtwhA2BfWvSAVTlKAAHKR8I_YhE328.png"
+  },
+  {
+    "productId": "1105",
+    "productName": "焦糖布丁",
+    "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/38/CtwhA2BfWvOAGGzDAAHk0XWqU1E328.png"
+  },
+  {
+    "productId": "1106",
+    "productName": "炸鸡翅",
+    "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/39/CtwhA2BfWvOABxJDAAHk0XWqU1E328.png"
+  },
+  {
+    "productId": "1107",
+    "productName": "薯条",
+    "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/40/CtwhA2BfWvSAYbGDAAHk0XWqU1E328.png"
+  },
+  {
+    "productId": "1108",
+    "productName": "春卷",
+    "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/41/CtwhA2BfWvKAGU0DAAHk0XWqU1E328.png"
+  },
+  {
+    "productId": "1109",
+    "productName": "披萨片",
+    "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/42/CtwhA2BfWvKADPsDAAHk0XWqU1E328.png"
+  },
+  {
+    "productId": "1110",
+    "productName": "牛肉干",
+    "thumbnailUrl": "https://img03.luckincoffeecdn.com/group1/M00/8D/43/CtwhA2BfWvCAWyLDAAHk0XWqU1E328.png"
+  }
+]
+
+// 热饮
+const arr6 = [
+  {
+    "productId": "200012",
+    "productName": "热巧克力",
+    "thumbnail": "hot_chocolate.jpg"
+  },
+  {
+    "productId": "200013",
+    "productName": "热牛奶",
+    "thumbnail": "hot_milk.jpg"
+  },
+  {
+    "productId": "200014",
+    "productName": "香料茶",
+    "thumbnail": "spiced_tea.jpg"
+  },
+  {
+    "productId": "200015",
+    "productName": "姜茶",
+    "thumbnail": "ginger_tea.jpg"
+  },
+  {
+    "productId": "200016",
+    "productName": "热苹果汁",
+    "thumbnail": "hot_apple_juice.jpg"
+  },
+  {
+    "productId": "200017",
+    "productName": "热柠檬水",
+    "thumbnail": "hot_lemon_water.jpg"
+  }
+]
+
